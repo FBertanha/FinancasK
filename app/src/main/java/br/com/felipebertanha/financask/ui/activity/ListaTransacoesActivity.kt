7c8b0f235c0c250bcece216bb25c18ea439f66d7
felipe <bertanhahp@hotmail.com>
2019-03-12 23:44:07 -0300

    commit inicial

---
+++ app/src/main/java/br/com/felipebertanha/financask/ui/activity/ListaTransacoesActivity.kt
@@ -0,0 +1,22 @@
+package br.com.felipebertanha.financask.ui.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import br.com.felipebertanha.financask.R
+import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+
+class ListaTransacoesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?): Unit {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_transacoes)
+
+        val lista = listOf("Comida R$ 20,50", "Economia - R$ 100,00")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, lista)
+
+        lista_transacoes_listview.setAdapter(arrayAdapter)
+    }
+
+}
